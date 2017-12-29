303905ce6a10f5d54eef7ad017caca5b6476a163
Waldo Jaquith <waldo@jaquith.org>
2017-12-28 23:25:29 -0500

    Move a bunch of functionality between files
    This makes more sense in handler.sh at this point.

--- bin/handler.sh
+++ bin/handler.sh
@@ -20,6 +20,35 @@ fi
 # Delete this video (and any other video lingering about).
 rm -f ./*.mp4
 
+############
+## TO DO
+############
+
+# Copy screenshots to S3.
+# If the sync worked properly, delete screenshots.
+if aws s3 sync . s3://video.richmondsunlight.com/"$CHAMBER"/floor/"$DATE" --exclude "*" --include "*.jpg"
+then
+	echo Deleting all local screenshots
+	rm ./*.jpg
+else
+	echo "AWS S3 sync didn't finish successfully, so screenshots were not thumbnailed or deleted"
+fi
+
+# Insert the chyrons into the database.
+#php parse_video.php "$VIDEO_ID"
+
+# Resolve the chyrons to individual legislators and bills.
+#php resolve_chyrons.php  "$VIDEO_ID"
+
+#/home/ubuntu/youtube-upload-master/bin/youtube-upload '
+#	. '--tags="virginia, legislature, general assembly" '
+#	. '--default-language="en" '
+#	. '--default-audio-language="en" '
+#	. '--title="Virginia ' . ucfirst($video['chamber']) . ', ' . date('F j, Y', strtotime($video['date'])) . '" '
+#	. '--recording-date="' . $DATE . 'T00:00:00.0Z" '
+#	. $DATE
+##
+
 # Run itself again, in case there are more videos in the queue.
 ./handler.sh
 
