01f7d06d21e8fae3e19e596bc3dd49250f107218
Waldo Jaquith <waldo@jaquith.org>
2017-12-30 23:17:12 -0500

    Deal with directories better

--- bin/handler.sh
+++ bin/handler.sh
@@ -3,22 +3,33 @@
 # Change to the directory containing this script.
 cd "$(dirname "$0")" || exit
 
+VIDEO_DIR="../video/"
+
+# Make a videos directory, if it doesn't already exist.
+mkdir -p $VIDEO_DIR
+
 # Retrieve the video, saving it to a file and to S3.
 php get_video.php || exit
 
 # Figure out the filename and chamber we're processing.
-VIDEO_FILE="$(ls "*.mp4")"
+cd $VIDEO_DIR || exit
+VIDEO_FILE="$(find ./*.mp4 |head -1)"
 if [[ "$VIDEO_FILE" == "house"* ]]; then
-	CHAMBER="house";
+	CHAMBER="house"
 else
-	CHAMBER="senate";
+	CHAMBER="senate"
 fi
 
-# Start the video processor.
-./process-video "$VIDEO_FILE" "$CHAMBER"
+# Remove the chamber from the filename.
+NEW_FILENAME="${$VIDEO_FILE/$CHAMBER-/}"
+mv "$VIDEO_FILE" "$NEW_FILENAME"
+VIDEO_FILE="$NEW_FILENAME"
 
-# Delete this video (and any other video lingering about).
-rm -f ./*.mp4
+# Define the name of the directory that will store the extracted chyrons.
+VIDEO_DIR="${$VIDEO_FILE/.mp4/}"
+
+# Start the video processor.
+../bin/process-video "$VIDEO_FILE" "$CHAMBER" || exit
 
 ############
 ## TO DO
@@ -26,6 +37,7 @@ rm -f ./*.mp4
 
 # Copy screenshots to S3.
 # If the sync worked properly, delete screenshots.
+cd "$VIDEO_DIR"
 if aws s3 sync . s3://video.richmondsunlight.com/"$CHAMBER"/floor/"$DATE" --exclude "*" --include "*.jpg"
 then
 	echo Deleting all local screenshots
@@ -51,6 +63,9 @@ php resolve_chyrons.php  "$VIDEO_ID"
 #	. '--recording-date="' . $DATE . 'T00:00:00.0Z" '
 #	. $DATE
 ##
+
+# Delete everything from the /video/ directory -- we're done with it.
+rm -Rf ../video/
 
 # Run itself again, in case there are more videos in the queue.
 ./handler.sh
