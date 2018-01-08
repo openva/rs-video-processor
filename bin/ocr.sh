670890b54b3c5ce6b67c453ce66c878a40171da4
Waldo Jaquith <waldo@jaquith.org>
2018-01-07 22:37:31 -0500

    Support House video chyrons
    This is work in progress. Toward #161.

--- bin/ocr.sh
+++ bin/ocr.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+####
+#### TO DO
+#### SET $date SO WE HAVE AN OUTPUT DIRECTORY
+#### SUPPORT SENATE COMMITTEE VIDEO...SOMEHOW
+#### SUPPORT SHORT-FORM HOUSE COMMITTEE VIDEO
+####
+
 # If the filename is missing, explain that it's required.
 if [ -z "$1" ]; then
 	echo "usage: $0 [YYYYMMDD.mp4] [chamber]"
@@ -66,13 +73,23 @@ fi
 if [ "$CHAMBER" = "house" ]; then
 	NAME_CROP="333x60+120+380"
 	BILL_CROP="129x27+465+42"
-else
+elif [ "$CHAMBER" = "senate" ]; then
+	NAME_CROP="345x60+176+340"
+	BILL_CROP="172x27+0+40"
+elif [ "$CHAMBER" = "house-committee" ]; then
+	NAME_AND_BILL_CROP="176x340+398+39"
+elif [ "$CHAMBER" = "senate-committee" ]; then
 	NAME_CROP="345x60+176+340"
 	BILL_CROP="172x27+0+40"
 fi
 
-for f in *[0-9].jpg; do convert "$f" -crop "$NAME_CROP" +repage -negate -fx '.8*r+.8*g+0*b' -compress none -depth 8 "$f".name.jpg; done
-for f in *[0-9].jpg; do convert "$f" -crop "$BILL_CROP" +repage -negate -fx '.8*r+.8*g+0*b' -compress none -depth 8 "$f".bill.jpg; done
+if "$NAME_CROP"; then
+	for f in *[0-9].jpg; do convert "$f" -crop "$NAME_CROP" +repage -negate -fx '.8*r+.8*g+0*b' -compress none -depth 8 "$f".name.jpg; done
+	for f in *[0-9].jpg; do convert "$f" -crop "$BILL_CROP" +repage -negate -fx '.8*r+.8*g+0*b' -compress none -depth 8 "$f".bill.jpg; done
+else
+
+	for f in *[0-9].jpg; do convert "$f" -crop "$NAME_AND_BILL_CROP" +repage -negate -fx '.8*r+.8*g+0*b' -compress none -depth 8 "$f".chyron.jpg; done
+fi	
 
 echo "OCRing names and bill numbers"
 
@@ -85,7 +102,7 @@ find . -type f -name '*.name.jpg' -exec rm {} \;
 find . -type f -name '*.bill.jpg' -exec rm {} \;
 
 # Duplicate all JPEGs with a -150 suffix.
-for F in $(ls -1 ./*.jpg |awk -F. '{print $2}')
+for F in $(find -1 ./*.jpg |awk -F. '{print $2}')
 do
 	cp ."$F".jpg ."${F}"-150.jpg
 done
