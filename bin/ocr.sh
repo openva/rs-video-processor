3acc758a6633cf38c20e5737b554a8b82635fbe5
Waldo Jaquith <waldo@jaquith.org>
2018-01-23 23:14:04 -0500

    Fix variable formatting error

--- bin/ocr.sh
+++ bin/ocr.sh
@@ -23,7 +23,7 @@ if [ ! -f "$SRC" ]; then
 	exit 1;
 fi
 
-if [[ ! -v OUTPUT_DIR ]]; then
+if [[ ! -v "$OUTPUT_DIR" ]]; then
 	echo "Error: output_dir is not set as an environment variable"
 	exit 1;
 fi
