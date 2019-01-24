cfd4ae58905fc2ecb9f5edef6c4977f9acafdc06
Waldo Jaquith <waldo@jaquith.org>
2019-01-23 20:36:55 -0500

    Set up mplayer's config file

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -8,3 +8,7 @@ if [ -z "$CRONTAB" ]
 then
     cat deploy/crontab.txt |sudo tee -a /etc/crontab
 fi
+
+# Move the mplayer config file to its proper location.
+[ -d ~/.mplayer/ ] || mkdir ~/.mplayer/
+mv -f ~/video-processor/deploy/mplayer-config ~/.mplayer/config
