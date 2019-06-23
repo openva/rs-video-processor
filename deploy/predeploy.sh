3454b2cb3065df88a8c59926c59da617f7db8855
Waldo Jaquith <waldo@jaquith.org>
2019-06-23 10:17:50 -0400

    Create swap on deploy

--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -2,6 +2,15 @@
 
 # Change the timezone to Eastern
 sudo cp /usr/share/zoneinfo/US/Eastern /etc/localtime
+    
+# Add swap space, if it doesn't exist
+if [ "$(grep -c swap /etc/fstab)" -eq "0" ]; then
+    sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+    sudo /sbin/mkswap /var/swap.1
+    sudo chmod 600 /var/swap.1
+    sudo /sbin/swapon /var/swap.1
+    echo "/var/swap.1   swap    swap    defaults        0   0" | sudo tee /etc/fstab
+fi
 
 # Remove all PHP packages (they may well be PHP 7)
 sudo apt-get -y purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
