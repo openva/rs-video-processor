aa9cb7f5c6cb25d0ce586cc01bcbdaf11f8fb78d
Waldo Jaquith <waldo@jaquith.org>
2020-02-13 22:04:19 -0500

    Run postdeploy when updating source

--- deploy/update_source.sh
+++ deploy/update_source.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
-cd ~; mkdir -p video-processor/ && cd video-processor/ && aws s3 cp s3://deploy.video.richmondsunlight.com/latest.zip latest.zip && unzip -o latest.zip
+cd ~
+mkdir -p video-processor/
+cd video-processor/
+aws s3 cp s3://deploy.video.richmondsunlight.com/latest.zip latest.zip
+unzip -o latest.zip
+
+# Run the postdeploy script
+deploy/postdeploy.sh
