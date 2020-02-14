326c28e1b5c97781a21f4af3590cf18364dba8f2
Waldo Jaquith <waldo@jaquith.org>
2020-02-13 22:03:58 -0500

    Fix postdeploy script path

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -9,6 +9,8 @@ then
     cat deploy/crontab.txt |sudo tee -a /etc/crontab
 fi
 
+cd ~
+
 # Move the mplayer config file to its proper location.
 [ -d ~/.mplayer/ ] || mkdir ~/.mplayer/
 mv -f ~/video-processor/deploy/mplayer-config ~/.mplayer/config
