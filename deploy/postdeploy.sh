4b8dabffdc126b1c42f0ad09a9429404475458b2
Waldo Jaquith <waldo@jaquith.org>
2020-07-07 14:21:07 -0400

    Fix failing crontab update

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu/video-processor/
 
 # Set up the crontab
-CRONTAB="$(video-processor /etc/crontab)"
+CRONTAB="$(grep video-processor /etc/crontab)"
 if [ -z "$CRONTAB" ]
 then
     cat deploy/crontab.txt |sudo tee -a /etc/crontab
