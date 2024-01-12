656c444c58de915f38fa700c3e307b3f6207f39e
Waldo Jaquith <waldo@jaquith.org>
2024-01-11 19:48:03 -0500

    Fix S3 path

--- deploy/update_source.sh
+++ deploy/update_source.sh
@@ -3,7 +3,7 @@
 cd ~
 mkdir -p video-processor/
 cd video-processor/
-aws s3 cp s3://deploy.richmondsunlight.com/rs-video-processor.zip rs-video-processor.zip
+aws s3 cp s3://deploy.richmondsunlight.com/rs-video-processor-master.zip rs-video-processor.zip
 unzip -o rs-video-processor.zip
 
 # Run the postdeploy script
