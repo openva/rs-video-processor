de6a9a9a48ccc3577a4a171cfd66796cda696e80
Waldo Jaquith <waldo@jaquith.org>
2024-01-11 19:40:33 -0500

    Fetch the source from a new location

--- deploy/update_source.sh
+++ deploy/update_source.sh
@@ -3,8 +3,8 @@
 cd ~
 mkdir -p video-processor/
 cd video-processor/
-aws s3 cp s3://deploy.video.richmondsunlight.com/latest.zip latest.zip
-unzip -o latest.zip
+aws s3 cp s3://deploy.richmondsunlight.com/rs-video-processor.zip rs-video-processor.zip
+unzip -o rs-video-processor.zip
 
 # Run the postdeploy script
 deploy/postdeploy.sh
