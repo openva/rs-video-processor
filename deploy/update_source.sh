9a5176ef005e93dcf8cc341eb867878eede8b60c
Waldo Jaquith <waldo@jaquith.org>
2024-01-11 20:52:06 -0500

    Remove the ZIP file when done

--- deploy/update_source.sh
+++ deploy/update_source.sh
@@ -5,6 +5,7 @@ mkdir -p video-processor/
 cd video-processor/
 aws s3 cp s3://deploy.richmondsunlight.com/rs-video-processor-master.zip rs-video-processor.zip
 unzip -o rs-video-processor.zip
+rm -f rs-video-processor.zip
 
 # Run the postdeploy script
 deploy/postdeploy.sh
