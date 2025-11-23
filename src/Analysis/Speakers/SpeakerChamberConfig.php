354a1ccbe6a1f3341d542d574beb84784e4334f7
Waldo Jaquith <waldo@jaquith.org>
2025-11-23 11:59:29 -0500

    Continue iterating on speaker crops

--- src/Analysis/Speakers/SpeakerChamberConfig.php
+++ src/Analysis/Speakers/SpeakerChamberConfig.php
@@ -13,9 +13,9 @@ class SpeakerChamberConfig
     {
         $this->configs = [
             'house_floor' => new CropConfig(0.02, 0.75, 0.55, 0.2),
-            'house_committee' => new CropConfig(0.02, 0.02, 0.45, 0.18),
+            'house_committee' => new CropConfig(0, 0.05, 0.15, 0.1),
             'senate_floor' => new CropConfig(0.0, 0.78, 1.0, 0.22),
-            'senate_committee' => new CropConfig(0.0, 0.70, 1.0, 0.25),
+            'senate_committee' => new CropConfig(0.07, 0.85, 0.93, 0.1),
         ];
     }
 
