2de765a99dbf5b389b2ca7c0fc4e6300f0c6d409
Waldo Jaquith <waldo@jaquith.org>
2025-11-25 23:56:24 -0500

    Remove duplicate function-loading

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use RichmondSunlight\VideoProcessor\Bootstrap\AppBootstrap;
 
 require_once __DIR__ . '/../includes/settings.inc.php';
-require_once __DIR__ . '/../includes/functions.inc.php';
 require_once __DIR__ . '/../includes/vendor/autoload.php';
 require_once __DIR__ . '/../includes/class.Database.php';
 
