fd44c984731c8411487c46e950e6cfece05a79c3
Waldo Jaquith <waldo@jaquith.org>
2026-01-15 12:38:11 -0500

    Fix failing tests

--- tests/Analysis/Bills/BillResultWriterTest.php
+++ tests/Analysis/Bills/BillResultWriterTest.php
@@ -27,7 +27,7 @@ class BillResultWriterTest extends TestCase
         $writer->record(1, 10, ['HB1234'], '00010.jpg');
 
         $row = $pdo->query('SELECT screenshot, raw_text, type FROM video_index')->fetch(PDO::FETCH_ASSOC);
-        $this->assertSame('00010.jpg', $row['screenshot']);
+        $this->assertSame('00010', $row['screenshot']);
         $this->assertSame('HB1234', $row['raw_text']);
         $this->assertSame('bill', $row['type']);
     }
@@ -52,9 +52,9 @@ class BillResultWriterTest extends TestCase
 
         $rows = $pdo->query('SELECT screenshot, raw_text FROM video_index ORDER BY id')->fetchAll(PDO::FETCH_ASSOC);
         $this->assertCount(2, $rows);
-        $this->assertSame('00010.jpg', $rows[0]['screenshot']);
+        $this->assertSame('00010', $rows[0]['screenshot']);
         $this->assertSame('HB1234', $rows[0]['raw_text']);
-        $this->assertSame('00010.jpg', $rows[1]['screenshot']);
+        $this->assertSame('00010', $rows[1]['screenshot']);
         $this->assertSame('SB567', $rows[1]['raw_text']);
     }
 }
