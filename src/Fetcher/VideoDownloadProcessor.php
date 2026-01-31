da050883ddf60cf04b8b3d62cae65c1e352f0f5f
Waldo Jaquith <waldo@jaquith.org>
2026-01-30 22:05:50 -0500

    Remove style from embed code

--- src/Fetcher/VideoDownloadProcessor.php
+++ src/Fetcher/VideoDownloadProcessor.php
@@ -395,7 +395,6 @@ class VideoDownloadProcessor
 <div class="video-embed-container" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%;">
     <iframe
         src="{$embedUrl}"
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen{$errorAttr}>
